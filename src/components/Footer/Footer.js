4267626dff38d25585f9f92522ea989db85b5a1d
Bohdan Hushcha <hushchabogdan@gmail.com>
2019-05-21 13:55:37 +0200

    Footer ready

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container, Col, Row } from "reactstrap";
+import classes from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={classes.Footer}>
+      <Container>
+        <Row>
+          <Col lg="12">
+            <div className={classes.Copyright}>Powered by PGS</div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;

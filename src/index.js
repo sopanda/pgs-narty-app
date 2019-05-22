7a4755897a956ebdf4f6c227ff392cbe052b18c8
Bohdan Hushcha <hushchabogdan@gmail.com>
2019-05-22 15:34:17 +0200

    Font fixes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./helpers";
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
